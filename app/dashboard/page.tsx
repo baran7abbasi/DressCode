dfaefda4ff919ef6ca2d323ceeee303a20e8191d
Katherine Sanders <68821698+katherinesanders@users.noreply.github.com>
2024-10-26 11:08:08 -0600

    Update page.tsx

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,29 +1,12 @@
-'use client';
 import { Button, Stack } from '@mantine/core';
-import { getUserId } from '../../utility/authUtility';
-import { useEffect, useState } from 'react';
+import { FeaturesTitle } from '../FeaturesTitle/FeaturesTitle';
+import { HeaderMegaMenu } from '../HeaderMegaMenu/HeaderMegaMenu';
 
 export default function Dashboard() {
-
 	return (
 		<Stack p={16}>
-			welcome to your closet
-			<Button
-				component='a'
-				href='/fit-generator'
-				style={{ alignSelf: 'center' }}
-			>
-				Generate a Fit
-			</Button>
-			<Button component='a' href='/closet' style={{ alignSelf: 'center' }}>
-				View your Closet
-			</Button>
-			<Button component='a' href='/ask-closet' style={{ alignSelf: 'center' }}>
-				Ask your closet!
-			</Button>
-			<Button component='a' href='/' style={{ alignSelf: 'center' }}>
-				Go Back to Login
-			</Button>
+			<HeaderMegaMenu/>
+			<FeaturesTitle />
 		</Stack>
 	);
 }
