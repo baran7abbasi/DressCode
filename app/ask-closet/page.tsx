051f4699ae35d6ea913e38089accba49e1246d66
Katherine Sanders <68821698+katherinesanders@users.noreply.github.com>
2024-10-26 17:58:49 -0600

    Update page.tsx

--- app/ask-closet/page.tsx
+++ app/ask-closet/page.tsx
@@ -1,10 +1,10 @@
-
+'use client';
 import { Button, Stack } from '@mantine/core';
 import { HeaderMegaMenu } from '../HeaderMegaMenu/HeaderMegaMenu';
 
 export default function AskCloset() {
 	return (
-		<Stack p={16}>
+		<Stack p={16} style={{ backgroundColor: '#000', minHeight: '100vh', color: 'white' }}>
 			<HeaderMegaMenu/>
 			ask your closet anything!
 		</Stack>
