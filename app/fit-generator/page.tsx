c14b6252aea3092603c4d75557963730c2615f9c
Katherine Sanders <68821698+katherinesanders@users.noreply.github.com>
2024-10-26 17:37:20 -0600

    Update page.tsx

--- app/fit-generator/page.tsx
+++ app/fit-generator/page.tsx
@@ -1,36 +1,12 @@
-'user: client';
-import { Stack, Grid } from '@mantine/core'; // Remove Col from imports
+import { Button, Stack } from '@mantine/core';
 import { ContainedInputs } from './ContainedInputs/ContainedInputs';
 import { HeaderMegaMenu } from '../HeaderMegaMenu/HeaderMegaMenu';
-import { BadgeCard } from './BadgeCard/BadgeCard';
 
 export default function FitGenerator() {
-  return (
-    <Stack p={16}>
-      <HeaderMegaMenu />
-      <div style={{ display: 'flex', width: '100%' }}>
-        <ContainedInputs/>
-
-        {/* Grid container for BadgeCard components */}
-        <Grid
-          style={{
-            marginLeft: '25px', // Space between inputs and cards
-            // display: 'grid',
-            // gridTemplateColumns: 'repeat(3, 1fr)', // 3 columns
-            gap: '10px', // Gap between cards
-          }}
-        >
-		<div style={{
-            margin:'10px'
-          }}><BadgeCard /><BadgeCard /></div>
-		<div style={{
-            margin:'10px'
-          }}><BadgeCard /><BadgeCard /></div>
-        <div style={{
-            margin:'10px'
-          }}><BadgeCard /><BadgeCard /></div>
-        </Grid>
-      </div>
-    </Stack>
-  );
+	return (
+		<Stack p={16}>
+			<HeaderMegaMenu/>
+			<ContainedInputs/>
+		</Stack>
+	);
 }
