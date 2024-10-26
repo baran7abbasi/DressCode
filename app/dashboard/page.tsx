9a0465219cb75a047dac05a082845ce1e2299b74
Katherine Sanders <68821698+katherinesanders@users.noreply.github.com>
2024-10-26 17:59:28 -0600

    Update page.tsx

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,11 +1,11 @@
 import { Button, Stack } from '@mantine/core';
-import { FeaturesTitle } from './FeaturesTitle/FeaturesTitle';
+import { FeaturesTitle } from '../FeaturesTitle/FeaturesTitle';
 import { HeaderMegaMenu } from '../HeaderMegaMenu/HeaderMegaMenu';
 
 export default function Dashboard() {
 	return (
-		<Stack>
-			<HeaderMegaMenu />
+		<Stack p={16} style={{ backgroundColor: '#000', minHeight: '100vh', color: 'white' }}>
+			<HeaderMegaMenu/>
 			<FeaturesTitle />
 		</Stack>
 	);
