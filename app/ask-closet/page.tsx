4e8c98818b4769011d7b2d5abaa76e08a40f32f1
Katherine Sanders <68821698+katherinesanders@users.noreply.github.com>
2024-10-26 20:45:09 -0600

    Update page.tsx

--- app/ask-closet/page.tsx
+++ app/ask-closet/page.tsx
@@ -4,7 +4,7 @@ import { HeaderMegaMenu } from '../HeaderMegaMenu/HeaderMegaMenu';
 
 export default function AskCloset() {
 	return (
-		<Stack p={16} style={{ backgroundColor: '#000', minHeight: '100vh', color: 'white' }}>
+		<Stack style={{ backgroundColor: '#000', minHeight: '100vh', color: 'white' }}>
 			<HeaderMegaMenu/>
 			ask your closet anything!
 		</Stack>
