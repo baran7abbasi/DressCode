084f1849b2abd75caefd4a5b4e953c268dfae5db
maddie-crowne <madscrowne14@gmail.com>
2024-10-26 21:38:53 -0400

    first commit (ask closet and generator and closet together)

--- app/ask-closet/page.tsx
+++ app/ask-closet/page.tsx
@@ -1,12 +1,15 @@
-
-import { Button, Stack } from '@mantine/core';
 import { HeaderMegaMenu } from '../HeaderMegaMenu/HeaderMegaMenu';
+import { Button, Stack } from '@mantine/core';
+import ChatPage from '../../components/ChatBox/ChatPage/page';
 
 export default function AskCloset() {
-	return (
-		<Stack p={16}>
-			<HeaderMegaMenu/>
-			ask your closet anything!
-		</Stack>
-	);
+  return (
+    <Stack p={16}>
+		<HeaderMegaMenu/>
+		<ChatPage/>
+		<Button component='a' href='/dashboard' style={{ alignSelf: 'center' }}>
+			Back to Home
+		</Button>
+    </Stack>
+  );
 }
