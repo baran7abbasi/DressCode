91c8acf4030faf4a50fc5c80e849d12c55bb949b
maddie-crowne <madscrowne14@gmail.com>
2024-10-27 04:22:16 -0400

    new updates

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,12 +1,14 @@
 import { Button, Stack } from '@mantine/core';
 import { FeaturesTitle } from './FeaturesTitle/FeaturesTitle';
 import { HeaderMegaMenu } from '../HeaderMegaMenu/HeaderMegaMenu';
+import NlXChatWidget from '../../components/ChatBox/ChatBubble/page';
 
 export default function Dashboard() {
 	return (
 		<Stack style={{ backgroundColor: '#000', minHeight: '100vh', color: 'white' }}>
 			<HeaderMegaMenu />
-			{/*<FeaturesTitle />*/}
+			{<FeaturesTitle />}
+			{<NlXChatWidget />}
 		</Stack>
 	);
 }
