b572a26756766ea1bf984c16d80d1178fa30ec35
b2abbasi <71993811+b2abbasi@users.noreply.github.com>
2024-10-27 06:29:06 -0400

    miscellaneous

--- app/closet/page.tsx
+++ app/closet/page.tsx
@@ -1,12 +1,16 @@
 import { Button, Stack } from '@mantine/core';
 import { NavbarSimpleColored } from '../../components/Closet-Navbar/NavbarSimpleColored';
 import { HeaderMegaMenu } from '../HeaderMegaMenu/HeaderMegaMenu';
+import NlXChatWidget from '../../components/ChatBox/ChatBubble/page';
 
 export default function Closet() {
 	return (
-		<Stack style={{ backgroundColor: '#000', minHeight: '100vh', color: 'white' }}>
-			<HeaderMegaMenu/>
+		<Stack
+			style={{ backgroundColor: '#000', minHeight: '100vh', color: 'white' }}
+		>
+			<HeaderMegaMenu />
 			<NavbarSimpleColored />
+			{<NlXChatWidget />}
 		</Stack>
 	);
 }
