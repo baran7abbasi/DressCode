2fe473b66fefdf219510572ac4e5e6d1474d9523
Katherine Sanders <68821698+katherinesanders@users.noreply.github.com>
2024-10-26 20:45:57 -0600

    Update page.tsx

--- app/ask-closet/page.tsx
+++ app/ask-closet/page.tsx
@@ -6,7 +6,6 @@ export default function AskCloset() {
 	return (
 		<Stack style={{ backgroundColor: '#000', minHeight: '100vh', color: 'white' }}>
 			<HeaderMegaMenu/>
-			ask your closet anything!
 		</Stack>
 	);
 }
